f8700f76d7d204d2eb964137493d1def9363b2bd
Alexandra Gazda <alexandragazda@yahoo.com>
2020-01-14 14:52:06 +0200

    routes done right

--- cinema-client/src/app/auth/auth-routing.module.ts
+++ cinema-client/src/app/auth/auth-routing.module.ts
@@ -1,57 +1,57 @@
-// import {RouterModule, Routes} from '@angular/router';
-// import {LoginComponent} from './login/login.component';
-// import {UserComponent} from '../user/user.component';
-// import {AdminComponent} from '../admin/admin.component';
-// import {ForgotPasswordComponent} from './forgot-password/forgot-password.component';
-// import {RegisterComponent} from './register/register.component';
-// import {SuccessfulRegistrationComponent} from './successful-registration/successful-registration.component';
-// import {ResetPasswordComponent} from './reset-password/reset-password.component';
-// import {NgModule} from '@angular/core';
-// import {CommonModule} from '@angular/common';
-//
-// const routes: Routes = [
-//   // {
-//   //   path: '',
-//   //   redirectTo: 'login',
-//   //   pathMatch: 'full'
-//   // },
-//   {
-//     path: 'login',
-//     component: LoginComponent,
-//   },
-//   {
-//     path: 'user',
-//     component: UserComponent
-//   },
-//   {
-//     path: 'admin',
-//     component: AdminComponent
-//   },
-//   {
-//     path: 'forgot-password',
-//     component: ForgotPasswordComponent
-//   },
-//   {
-//     path: 'register',
-//     component: RegisterComponent
-//   },
-//   {
-//     path: 'successful-registration',
-//     component: SuccessfulRegistrationComponent,
-//   },
-//   {
-//     path: 'reset-password',
-//     component: ResetPasswordComponent,
-//   },
-// ];
-//
-// @NgModule({
-//   declarations: [],
-//   imports: [
-//     CommonModule,
-//     RouterModule.forChild(routes)
-//   ],
-//   exports: [RouterModule]
-// })
-// export class AuthRoutingModule {
-// }
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {LoginComponent} from './login/login.component';
+import {UserComponent} from '../user/user.component';
+import {RouterModule, Routes} from '@angular/router';
+import {AdminComponent} from '../admin/admin.component';
+import {ForgotPasswordComponent} from './forgot-password/forgot-password.component';
+import {RegisterComponent} from './register/register.component';
+import {SuccessfulRegistrationComponent} from './successful-registration/successful-registration.component';
+import {ResetPasswordComponent} from './reset-password/reset-password.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'my-account',
+    component: UserComponent
+  },
+  {
+    path: 'admin',
+    component: AdminComponent
+  },
+  {
+    path: 'forgot-password',
+    component: ForgotPasswordComponent
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
+  },
+  {
+    path: 'successful-registration',
+    component: SuccessfulRegistrationComponent,
+  },
+  {
+    path: 'reset-password',
+    component: ResetPasswordComponent,
+  },
+];
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes)
+  ],
+  exports: [RouterModule]
+})
+
+export class AuthRoutingModule { }
---
+++ cinema-client/src/app/movie/movie-routing.module.ts
@@ -0,0 +1,18 @@
+// import { NgModule } from '@angular/core';
+// import { CommonModule } from '@angular/common';
+// import {RouterModule, Routes} from '@angular/router';
+// import {MovieListComponent} from './movie-list/movie-list.component';
+// import {MovieItemComponent} from './movie-list/movie-item/movie-item.component';
+//
+// const routes: Routes = [
+//   {
+//     path:
+//   }
+// ]
+// @NgModule({
+//   declarations: [],
+//   imports: [
+//     CommonModule
+//   ]
+// })
+// export class MovieRoutingModule { }
